9e13442cbac318389bb3b097348916d9177ce567
Hudson Clementino <hudson24m@gmail.com>
2024-08-19 10:02:40 -0300

    Implementação da classe position

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,5 +1,17 @@
 package boardgame;
 
 public class Position {
+	
+	private int coluna;
+	private int linha;
+	
+	
+	public Position(int coluna, int linha) {
+		
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+	
+	
 
 }
